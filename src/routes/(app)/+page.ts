aaa1417676af01fcab1db368addef7d8a78c64a9
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-02-15 05:00:33 +0000

    feat: new data structure and some refactor

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,4 +1,5 @@
-import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { getSupabase, type TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit';
+import type { Session } from '@supabase/supabase-js';
 import type { PageLoad } from './$types';
 
 export const load = (async (event) => {
@@ -7,40 +8,66 @@ export const load = (async (event) => {
 	let movements: IMovementTransaction[] = [];
 
 	if (session) {
-		const now = new Date();
-		const nowStr = now.toISOString().slice(0, 'yyyy-mm-dd'.length);
-
-		const { data, error } = await supabaseClient
-			.from('movements')
-			.select()
-			.eq('user_id', session.user.id)
-			.gte('created_at', `${nowStr} 00:00:00`)
-			.lte('created_at', `${nowStr} 23:59:59`);
-
-		if (error) {
-			console.error(error);
-			throw error;
-		}
-
-		movements = data ?? [];
+		const supabaseMovement = await getFromSupabase(session, supabaseClient);
+		movements = supabaseMovement ?? [];
 	}
 
 	if (typeof localStorage !== 'undefined') {
-		const previousMovements: IMovementTransaction[] = JSON.parse(
-			localStorage.getItem('movements') ?? '[]'
-		);
-
-		const unaddedMovements = previousMovements.filter((m) =>
+		const processedMovements = getFromLocalStorage().filter((m) =>
 			movements.every((alreadyAdded) => alreadyAdded.id !== m.id)
 		);
 
-		movements = [...movements, ...unaddedMovements];
+		movements = [...movements, ...processedMovements];
 	}
 
+	const groupKeys = new Set<string>();
+	const groupedMovements = new Map<string, IMovementTransaction[]>();
+
+	movements.forEach((movement) => {
+		const normalizedKey = movement.name.trim().toLowerCase();
+		groupKeys.add(normalizedKey);
+
+		if (groupedMovements.has(normalizedKey)) {
+			groupedMovements.get(normalizedKey)?.push(movement);
+		} else {
+			groupedMovements.set(normalizedKey, [movement]);
+		}
+	});
+
 	return {
-		movements
+		movements,
+		groupedMovements,
+		groupKeys: [...groupKeys]
 	};
 }) satisfies PageLoad;
+
+async function getFromSupabase(session: Session, supabaseClient: TypedSupabaseClient) {
+	const now = new Date();
+	const nowStr = now.toISOString().slice(0, 'yyyy-mm-dd'.length);
+
+	const { data, error } = await supabaseClient
+		.from('movements')
+		.select()
+		.eq('user_id', session.user.id)
+		.gte('created_at', `${nowStr} 00:00:00`)
+		.lte('created_at', `${nowStr} 23:59:59`)
+		.order('created_at', { ascending: true });
+
+	if (error) {
+		console.error(error);
+		throw error;
+	}
+
+	return data;
+}
+
+function getFromLocalStorage() {
+	const previousMovements: IMovementTransaction[] = JSON.parse(
+		localStorage.getItem('movements') ?? '[]'
+	);
+
+	return previousMovements;
+}
 
 interface IMovementTransaction {
 	id: string;
