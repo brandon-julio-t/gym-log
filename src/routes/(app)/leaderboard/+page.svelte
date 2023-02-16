26830c799b4bc4a9ebc340bf23f6a8fd24a0f513
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-02-16 09:58:12 +0000

    feat: leaderboard

---
+++ src/routes/(app)/leaderboard/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table-zebra table w-full">
+		<thead>
+			<tr>
+				<th>Rank</th>
+				<th>User</th>
+				<th>Movement</th>
+				<th>Reps</th>
+				<th>Weight</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.movements as movement, i}
+				<tr>
+					<th>
+						#{i + 1}
+
+						{#if i === 0}
+							👑
+						{:else if i === 1}
+							🥈
+						{:else if i === 2}
+							🥉
+						{/if}
+					</th>
+					<td>{movement.username}</td>
+					<td>{movement.name}</td>
+					<td>{movement.reps} times</td>
+					<td>{movement.weight} kg</td>
+				</tr>
+			{:else}
+				<tr>
+					<th colspan="5">No data.</th>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
---
+++ src/routes/(app)/leaderboard/+page.ts
@@ -0,0 +1,52 @@
+import { MOVEMENTS_ENTITY_NAME } from '$lib/constants';
+import { getSupabase, type TypedSupabaseClient } from '@supabase/auth-helpers-sveltekit';
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load = (async (event) => {
+	const { session, supabaseClient } = await getSupabase(event);
+
+	if (!session) throw redirect(303, '/login');
+
+	const profiles = await getProfiles(supabaseClient);
+
+	const { data, error } = await supabaseClient
+		.from(MOVEMENTS_ENTITY_NAME)
+		.select()
+		.order('weight', { ascending: false })
+		.order('reps', { ascending: false })
+		.limit(10);
+
+	if (error) {
+		console.error(error);
+		throw error;
+	}
+
+	const movements = data.map((movement) => {
+		const [profile] = profiles.filter((p) => p.id === movement.user_id);
+
+		if (profile?.full_name) {
+			const censored = profile.full_name.slice(0, 3) + '********';
+			movement.username = censored;
+		} else {
+			movement.username = 'Anonymous';
+		}
+
+		return movement;
+	});
+
+	return {
+		movements
+	};
+}) satisfies PageLoad;
+
+async function getProfiles(supabaseClient: TypedSupabaseClient) {
+	const { data, error } = await supabaseClient.from('profiles').select();
+
+	if (error) {
+		console.error(error);
+		throw error;
+	}
+
+	return data;
+}
