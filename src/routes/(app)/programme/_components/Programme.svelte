a4fb2f236ce6a2828a42ea50f77001a1ac0e2e0d
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-02-17 07:17:49 +0000

    fix: add more space in mobile by hiding the arrow button when editing

--- src/routes/(app)/programme/_components/Programme.svelte
+++ src/routes/(app)/programme/_components/Programme.svelte
@@ -142,46 +142,48 @@
 								{movement}
 							{/if}
 						</div>
-						<div class="flex space-x-2">
-							<button
-								on:click={() => handleChangeMovementOrder(programme, movement, -1)}
-								class="btn-outline btn-ghost btn-square btn"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="h-6 w-6"
+						{#if !programmeToBeEdited}
+							<div class="flex space-x-2">
+								<button
+									on:click={() => handleChangeMovementOrder(programme, movement, -1)}
+									class="btn-outline btn-ghost btn-square btn"
 								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M4.5 15.75l7.5-7.5 7.5 7.5"
-									/>
-								</svg>
-							</button>
-							<button
-								on:click={() => handleChangeMovementOrder(programme, movement, +1)}
-								class="btn-outline btn-ghost btn-square btn"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="h-6 w-6"
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="h-6 w-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M4.5 15.75l7.5-7.5 7.5 7.5"
+										/>
+									</svg>
+								</button>
+								<button
+									on:click={() => handleChangeMovementOrder(programme, movement, +1)}
+									class="btn-outline btn-ghost btn-square btn"
 								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-									/>
-								</svg>
-							</button>
-						</div>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="h-6 w-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+										/>
+									</svg>
+								</button>
+							</div>
+						{/if}
 					</div>
 				{:else}
 					<div>No movement yet.</div>
